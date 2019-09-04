f5da591b4647f29f1fffee401fb623db8d0726d7
Adam <adamchen0510@gmail.com>
2019-09-04 10:49:42 +0800

    Add read csv to str and util.py

--- cleanzhubi.py
+++ cleanzhubi.py
@@ -5,6 +5,7 @@ import sys
 import gzip
 import os
 import pandas as pd
+import util
 
 if __name__ == "__main__":
     if (len(sys.argv) < 3):
@@ -19,15 +20,27 @@ if __name__ == "__main__":
     df = pd.DataFrame(columns=("exchange_time", "contract", "price", "bs", "amount", "exchange_timestamp", "time", "timestamp"))
     for i in range(0, len(file_list)):
         path = os.path.join(data_dir, file_list[i])
-        if os.path.isfile(path) and not path.endswith(".gz"):
+        if util.is_zhubi_raw_file(path):
             print(f"file is:{path}")
-            df_temp = pd.read_csv(path, names=["exchange_time", "contract", "price", "bs", "amount", "exchange_timestamp", "time", "timestamp"])
+            df_temp = pd.read_csv(path, dtype=str, names=["exchange_time", "contract", "price", "bs", "amount", "exchange_timestamp", "time", "timestamp"])
+
+            # 删除有空值的行
+            df_temp = df_temp.dropna()
+
             df = pd.concat([df, df_temp])
             for row in df.itertuples():
-                # check price and volume
+                row_time = getattr(row, "time")
+                if row_time == "time":
+                    df.drop(index=[row[0], row[0]], inplace=True)
+                    continue
+
                 price = getattr(row, "price")
                 volume = getattr(row, "amount")
-                if price <= 0 or volume <= 0.0:
+
+                # check price and volume
+                if price <= "0.0" or volume <= "0.0":
                     print(f"unexpected price: {price}, volume: {volume}, row: {row[0]}")
                     df.drop(index=[row[0], row[0]], inplace=True)
+
+                # TODO: check price diff abnormal
     df.to_csv(output, index=False)
