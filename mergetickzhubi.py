9172deb48bbdb5494f3b1013c8b368e9f9f8176f
Adam <adamchen0510@gmail.com>
2019-09-04 13:40:44 +0800

    Merge zhubi and tick data to one csv

---
+++ mergetickzhubi.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+import math
+import sys
+import gzip
+import os
+import pandas as pd
+import util
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print(f"Usage python mergetickzhubi.py $tick.csv $zhubi.csv $outputFile")
+        exit(0)
+    tick_csv = sys.argv[1]
+    zhubi_csv = sys.argv[2]
+    output = sys.argv[3]
+    print(f"tick_csv: {tick_csv} zhubi_csv: {zhubi_csv}")
+
+    df_tick = pd.read_csv(tick_csv, dtype=str, names=["time", "timestamp", "last", "volume", "ask_0_p", "ask_0_v", "bid_0_p", "bid_0_v"])
+    df_zhubi = pd.read_csv(zhubi_csv, dtype=str, names=["exchange_time", "contract", "price", "bs", "amount", "exchange_timestamp", "time", "timestamp"])
+
+    df_zhubi_start_index = 0
+    for tick_row in df_tick.itertuples():
+        tick_time = getattr(tick_row, "time")
+        if tick_time == "time":
+            df_tick.drop(index=[tick_row[0], tick_row[0]], inplace=True)
+            continue
+        zhubi_total_volume = 0.0
+        tick_v = float(getattr(tick_row, "volume"))
+        if tick_v > 0.0:
+            tick_time = getattr(tick_row, "time")
+            # for zhubi_row in df_zhubi.itertuples():
+            for zhubi_row in df_zhubi[df_zhubi_start_index:]:
+                print(f"zhubi_row: {zhubi_row}")
+                zhubi_time = getattr(zhubi_row, "time")
+                if zhubi_time == "time":
+                    df_zhubi.drop(index=[zhubi_row[0], zhubi_row[0]], inplace=True)
+                    continue
+                if zhubi_row[0] < df_zhubi_start_index:
+                    continue
+                if tick_time < zhubi_time:
+                    if not math.isclose(tick_v, zhubi_total_volume, abs_tol=0.0000001):
+                        print(f"unexpected tick v: {tick_v}, total_zhubi_v: {zhubi_total_volume}")
+                        exit(0)
+                    else:
+                        print(f"inexpected tick v: {tick_v}, total_zhubi_v: {zhubi_total_volume}")
+                    df_zhubi_start_index = zhubi_row[0]
+                    break
+                zhubi_total_volume = zhubi_total_volume + float(getattr(zhubi_row, "amount"))
+                # print(f"zhubi_total_volume: {zhubi_total_volume}")
+
+    # df.to_csv(output, index=False)
